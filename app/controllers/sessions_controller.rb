41197356d82839feb3aff1932f6664e930ec67cc
Hadiyah Mujhid <diyahm@aol.com>
2012-02-05 01:42:43 -0800

    fix image

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,11 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     @client = Foursquare2::Client.new(:oauth_token => current_user.access_token)
+    results = @client.user(user.uid)
+      user.first_name = results["firstName"]
+      user.last_name = results["lastName"]
+      user.image = results["photo"]
+      user.save
     redirect_to dash_path, :notice => "Signed in!"
   end
   
